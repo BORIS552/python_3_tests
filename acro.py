23b4c863c38ed8cdad2160386172a94c54239418
BORIS552 <boris.biswas@gmail.com>
2021-02-03 23:26:18 +0530

    sol

--- acro.py
+++ acro.py
@@ -1,23 +1,30 @@
+
+def acronym(input):
+    s = input
+    final = ''
+    count = 0
+    res = []
+    for x in s:
+        for ch in x:
+            if ch.isupper():
+                res.append(ch)
+        # res = [char for char in x if char.isupper()]
+
+    for y in res:
+        if(count < len(s)-1):
+            final += y+'.'
+        else:
+            final += y
+        count = count + 1
+    
+    return final
+
+
 s = ['What','You']
 
-print(list(s))
-s = list(s)
-final = ''
-count = 0
-res = []
-for x in s:
-    print(x)
-    for ch in x:
-        if ch.isupper():
-            print(ch)
-            res.append(ch)
-    # res = [char for char in x if char.isupper()]
+print(acronym(list(s)))
 
-for y in res:
-    if(count < len(s)-1):
-        final += y+'.'
-    else:
-        final += y
-    count = count + 1
 
-print(final)
+
+
+
