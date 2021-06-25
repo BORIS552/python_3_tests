9112a645be6bf470f666363b1459d321ec10cc66
boris552 <boris.biswas@gmail.com>
2021-06-25 23:19:16 +0530

    up

--- findnumber.py
+++ findnumber.py
@@ -1,5 +1,12 @@
-try:
-    arr.index(k)
-    print('YES')
-except:
-    print('NO')
+
+def findnumber(arr, k):
+    try:
+        arr.index(k)
+        return 'YES'
+    except:
+        return 'NO'
+
+if __name__ == "__main__":
+    arr = [1,2,3,4,5]
+    val = findnumber(arr, 10)
+    print(val)
